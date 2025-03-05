7d4ef56be017e2418e0b4e920e0d9000d104f6eb
Seu Nome <mandoqxo@gmail.com>
2025-03-05 19:43:32 -0300

    Realizando o testes de imput no programa

--- src/ContaBanco/ContaTerminal.java
+++ src/ContaBanco/ContaTerminal.java
@@ -1,5 +1,7 @@
 package ContaBanco;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
         
@@ -9,7 +11,15 @@ public class ContaTerminal {
         double Saldo = Double.parseDouble(args[3]);
 
 
-        
+        Scanner teclado = new Scanner(System.in);
+        System.out.printf("Digite o numero da agencia: %s\n", Agencia);
+        String agencia = teclado.nextLine();
+        System.out.println("Digite o nome do usuario: %s\n" + NomeClinte);
+        String nome = teclado.nextLine();
+        System.out.println("Digite o saldo da conta: %f\n" + Saldo);
+
+
+
 
 
 
