6cf2f2df83e743ed18f5b86c4097d258a4d447b3
Seu Nome <mandoqxo@gmail.com>
2025-03-05 19:39:07 -0300

    Um tema relacionado ao estudo em java usando o terminal. Criando uma simulaçao de um sistema bancario.

---
+++ src/ContaBanco/ContaTerminal.java
@@ -0,0 +1,20 @@
+package ContaBanco;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        int NumeroConta = Integer.parseInt(args[0]);
+        String Agencia = args[1];
+        String NomeClinte = args[2];
+        double Saldo = Double.parseDouble(args[3]);
+
+
+        
+
+
+
+
+
+
+    }
+}
